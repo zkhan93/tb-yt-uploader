3346b43f79cc0177d75649d9a537224d40dc78c3
Zeeshan Khan <zkhan1093@gmail.com>
2023-02-02 15:08:10 -0500

    fix task model

--- web/app/models.py
+++ web/app/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel as PydanticBase, Extra
-from typing import Optional
+from typing import Optional, Any
 
 class BaseModel(PydanticBase):
     class Config:
@@ -34,4 +34,4 @@ class TaskResult(BaseModel):
 class TaskStatus(BaseModel):
     id: str
     status: str
-    result: Optional[TaskResult | str] 
+    result: Optional[Any] 
