5ab642e9f316fb6da302415ea18674cc8103df5f
Zeeshan Khan <zkhan1093@gmail.com>
2023-02-06 15:03:42 -0500

    (feat ptb) improved to accept email and title

--- ptb/handlers/convert.py
+++ ptb/handlers/convert.py
@@ -11,48 +11,88 @@ import time
 logger = logging.getLogger(__name__)
 
 
-def convert(update, context):
-    config = get_config()
+def send_error(update, context, message):
+    send_message(update, context, message, parse_mode=ParseMode.MARKDOWN_V2)
+
+
+def send_message(update, context, message, **kwargs):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message, **kwargs)
+
+
+def get_email_title(update, context):
+    args = update.effective_message.text.lstrip("/convert ")
+    email, title = None, None
+    try:
+        email, title = args.split(" ", 1)
+        logger.info(f"{email} {title}")
+    except Exception as ex:
+        logger.exception("error parsing email and title ")
+        send_error(
+            update, context, f"error parsing email and/or title: ```\n{str(ex)}\n```"
+        )
+    return email, title
+
+
+def get_download_audio_path(update, context):
     audio_message = update.effective_message.reply_to_message
-    title = update.effective_message.text.lstrip("/convert ")
-    logger.info(title)
+    config = get_config()
+    audio_path = None
     try:
         audio_path = download_audio(config, audio_message)
     except Exception as ex:
-        logger.exception("could not extract audio file")
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"error fetching audio: ```\n{str(ex)}\n```",
-            parse_mode=ParseMode.MARKDOWN_V2,
-        )
-    else:
-        try:
-            task_id = submit_audio(audio_path, title=title)
-        except Exception as ex:
-            logger.exception("Error while submitting task")
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"error submitting task: ```\n{str(ex)}\n```",
-                parse_mode=ParseMode.MARKDOWN_V2,
-            )
-        else:
-            # poll status of this task_id
-            status = poll_task_status(task_id)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"uploaded: ```\n{status}\n```",
-                parse_mode=ParseMode.MARKDOWN_V2,
-            )
-        finally:
-            delete_file(audio_path)
+        logger.exception("error while downloading audio file")
+        send_error(update, context, f"error downloading audio: ```\n{str(ex)}\n```")
+    return audio_path
 
-def poll_task_status(task_id, delay:int=3):
-    res = {}
-    while res.get("status") not in ("SUCCESS", "FAILURE", "REVOKED"):
-        res = get_task_status(task_id)
-        time.sleep(delay)
+
+def submit_task(update, context, audio_path, email, title):
+    task_id = None
+    try:
+        task_id = submit_audio(audio_path, title=title)
+    except Exception as ex:
+        logger.exception("Error while submitting task")
+        send_error(update, context, f"error submitting task: ```\n{str(ex)}\n```")
+    return task_id
+
+
+def convert(update, context):
+
+    email, title = get_email_title(update, context)
+    if not email or not title:
+        return
+
+    audio_path = get_download_audio_path(update, context)
+    if not audio_path:
+        return
+
+    task_id = submit_task(update, context, audio_path, email, title)
+    if not task_id:
+        return
+
+    # poll status of this task_id
+    status = poll_task_status(update, context, task_id)
+    if not status:
+        return
+
+    send_error(update, context, f"uploaded: ```\n{status}\n```")
+    delete_file(audio_path)
+
+
+def poll_task_status(
+    update,
+    context,
+    task_id,
+    delay: int = 3,
+):
+    res = None
+    try:
+        while res.get("status") not in ("SUCCESS", "FAILURE", "REVOKED"):
+            res = get_task_status(task_id)
+            time.sleep(delay)
+    except Exception as ex:
+        logger.exception("Error while polling task status")
+        send_error(update, context, f"error submitting task: ```\n{str(ex)}\n```")
     return res
-
 
 
 def get_convert_handler(config):
